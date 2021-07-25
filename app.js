d75c912613f996a24f194a4f4fe96042876d590f
Luca <lucacredentino98@gmail.com>
2021-07-26 00:54:40 +0200

    My Calculator v.1.1

--- app.js
+++ app.js
@@ -1,41 +1,82 @@
 const NumbersAndOperator = document.querySelectorAll('.btn');
 const EqualBtn = document.querySelector('.btn-equal');
 const CancelBtn = document.querySelector('.btn-clear');
-const screen = document.querySelector('.screen');
-const result = document.querySelector('.result');
+const screenDisplay = document.querySelector('.screen');
+const resultDisplay = document.querySelector('.result');
 const sunLogo = document.querySelector('.sun-logo');
 const moonLogo = document.querySelector('.moon-logo');
 const section = document.querySelector('.calculator')
+const operatorBtn = document.querySelectorAll('.operatorBtn')
 
     NumbersAndOperator.forEach((Buttons)=>{
         Buttons.addEventListener('click', (Buttons)=>{
             var valueOfButtons = Buttons.currentTarget.dataset.num;
-            screen.value += valueOfButtons;
+            screenDisplay.value += valueOfButtons;
      })
     })
 
     CancelBtn.addEventListener('click', ()=>{
-        screen.value = "";
-        result.value = "";
+        screenDisplay.value = "";
+        resultDisplay.value = "";
     })
 
     EqualBtn.addEventListener('click', ()=>{
-        if(screen.value === ""){
-            screen.value = 'Inserisci dei valori'
+        if (screenDisplay.value === "") {
+            screenDisplay.value = "Inserisci un valore"
         }else{
-        let Output = eval(screen.value);
-        result.value = Output;
-    }//else
-    });
+            let risultato = eval(screenDisplay.value);
+            resultDisplay.value = risultato;
+        };
+    })
+
+    //Feature: Light & Night mode.
 
        moonLogo.addEventListener('click', ()=>{
-            section.style.backgroundColor = "black"
-            NumbersAndOperator.style.backgroundColor = "black"
-            NumbersAndOperator.style.color = "white"
+         sunLogo.style.color = "orange"
+           moonLogo.style.color = "black"
+           //cambio background-color della calcolatrice
+            section.style.backgroundColor = "black";
+            //cambio colore dei Buttons
+            NumbersAndOperator.forEach((buttons)=>{
+                buttons.style.backgroundColor = "black";
+                buttons.style.color = "white";
+            });
+            //Cambio colore dei bottone "="
+            EqualBtn.style.backgroundColor = "black";
+            EqualBtn.style.color = "white";
+            //Screen Risultato cambio colore
+            screenDisplay.style.color = "white";
+            screenDisplay.style.backgroundColor = "black";
+            //Screen Risultato cambio colore
+            resultDisplay.style.color = "#E8E8E8";
+            resultDisplay.style.backgroundColor =  "black";
+            //Btn cancella cambio colore scritta
+            CancelBtn.style.color=" red";
     })
+
     sunLogo.addEventListener('click', ()=>{
-        section.style.backgroundColor = '#E8E8E8'
-        moonLogo.className.add('none')
+        sunLogo.style.color = "#E8E8E8"
+        //cambio background-color della calcolatrice
+        section.style.backgroundColor = '#E8E8E8';
+        //cambio colore dei Buttons
+        NumbersAndOperator.forEach((buttons)=>{
+            buttons.style.backgroundColor = "#E8E8E8";
+            buttons.style.color = "black";
+        });
+        operatorBtn.forEach((buttons)=>{
+            buttons.style.backgroundColor = "#BBBFCA";
+        });
+        //Btn cancella cambio colore scritta
+        CancelBtn.style.color=" red";
+        //Screen espressione cambio colore
+        screenDisplay.style.color = "black";
+        screenDisplay.style.backgroundColor = "#E8E8E8";
+        //Screen Risultato cambio colore
+        resultDisplay.style.color = "black";
+        resultDisplay.style.backgroundColor =  "#E8E8E8";
+        //Btn uguale cambio colore
+        EqualBtn.style.backgroundColor = "#E8E8E8";
+        EqualBtn.style.color = "black";
     });
 
 
